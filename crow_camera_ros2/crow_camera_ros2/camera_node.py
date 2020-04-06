8b49d4ebe515269f8c83a2c04d0f7f49ccac3abf
Radoslav Skoviera <radoslav.skoviera@cvut.cz>
2020-04-06 10:38:04 +0200

    Added vision&camera ros2 pkgs

---
+++ crow_camera_ros2/crow_camera_ros2/camera_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from crow_camera_ros2.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ crow_vision_ros2/crow_vision_ros2/detector.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from crow_vision_ros2.')
+
+
+if __name__ == '__main__':
+    main()
