8557c7c6bdcb81c1bb2d5cd11fc30046734dbe95
Marek Otahal <markotahal@gmail.com>
2020-05-15 12:47:16 +0200

    ROS: vision node WIP

--- ros2/src/crow_vision_ros2/crow_vision_ros2/detector.py
+++ ros2/src/crow_vision_ros2/crow_vision_ros2/detector.py
@@ -1,5 +1,20 @@
-def main():
+import rclpy #add to package.xml deps
+from rclpy.node import Node
+import sensor_msgs
+from cv_bridge import CvBridge
+import cv2
+
+class CrowVision(Node):
+  """
+  ROS2 node for CNN used in Crow. 
+  """
+
+  def __init__(self):
     print('Hi from crow_vision_ros2.')
+
+
+def main():
+  cnn = CrowVision()
 
 
 if __name__ == '__main__':
