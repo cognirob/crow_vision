4680948354d12e3c1887b33358ecb3d1b6f71b86
Radoslav Skoviera <radoslav.skoviera@cvut.cz>
2020-06-17 17:22:14 +0200

    Added all camera launch file

--- ros2/src/crow_vision_ros2/setup.py
+++ ros2/src/crow_vision_ros2/setup.py
@@ -7,9 +7,9 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['launch/all_cameras.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
