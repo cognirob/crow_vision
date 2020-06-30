462df462fd74547ca32d11504e0d490b9e24cdb6
Radoslav Skoviera <radoslav.skoviera@cvut.cz>
2020-06-30 17:11:16 +0200

    Easier launch param specification

--- ros2/src/crow_vision_ros2/launch/all_cameras.launch.py
+++ ros2/src/crow_vision_ros2/launch/all_cameras.launch.py
@@ -34,20 +34,27 @@ def generate_launch_description():
     camera_configs = []
     for i, device in enumerate(rs.context().devices):
         if device.get_info(rs.camera_info.name).lower() != 'platform camera':
-            camera_serial_no = LaunchConfiguration('serial_no', default=device.get_info(rs.camera_info.serial_number))
-            camera_base_frame_id = LaunchConfiguration('base_frame_id', default=f'camera{i + 1}_link')
+            launchParams = {'align_depth': True,
+                            'enable_pointcloud': True,
+                            'dense_pointcloud': True,
+                            }
+
+            launchParams['serial_no'] = LaunchConfiguration('serial_no', default=device.get_info(rs.camera_info.serial_number))
+            launchParams['base_frame_id'] = LaunchConfiguration('base_frame_id', default=f'camera{i + 1}_link')
+            # launchParams['depth_optical_frame_id'] = LaunchConfiguration('depth_optical_frame_id', default=f'camera{i + 1}_depth_optical_frame')
+            # launchParams['camera_infra1_optical_frame'] = LaunchConfiguration('camera_infra1_optical_frame', default=f'camera{i + 1}_infra1_optical_frame')
+            # launchParams['camera_infra2_optical_frame'] = LaunchConfiguration('camera_infra2_optical_frame', default=f'camera{i + 1}_infra2_optical_frame')
+            # launchParams['camera_color_optical_frame'] = LaunchConfiguration('camera_color_optical_frame', default=f'camera{i + 1}_color_optical_frame')
+            # launchParams['camera_accel_optical_frame'] = LaunchConfiguration('camera_accel_optical_frame', default=f'camera{i + 1}_accel_optical_frame')
+            # launchParams['camera_gyro_optical_frame'] = LaunchConfiguration('camera_gyro_optical_frame', default=f'camera{i + 1}_gyro_optical_frame')
+
 
             camera_node = Node(
                 package='realsense_node',
                 node_executable='realsense_node',
                 node_namespace=f"/camera{i + 1}",
                 output='screen',
-                parameters=[{'serial_no': camera_serial_no,
-                             'base_frame_id': camera_base_frame_id,
-                             'align_depth': True,
-                             'enable_pointcloud': True,
-                             'dense_pointcloud': True,
-                             }]
+                parameters=[launchParams]
             )
             camera_configs.append(camera_node)
     return launch.LaunchDescription(camera_configs)
