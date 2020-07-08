2374cb2e5e7e56c6970812dcc7de9db82c7d2b5b
Marek Otahal <markotahal@gmail.com>
2020-07-08 15:02:30 +0200

    tiny cleanup to yolact cluster train script
    making it more ready to use.

--- external/yolact/train_cluster.sh
+++ external/yolact/train_cluster.sh
@@ -3,19 +3,23 @@
 #SBATCH --output=yolact_job.out
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:3
-#SBATCH --mem=12G
-#SBATCH --time=00:03:00
+#SBATCH --mem=36G
+#SBATCH --time=72:00:00
 #SBATCH --partition=gpu
 
 ## To be run by sbatch on CIIRC cluster
 # you need to create conda env yolact-env beforehand. 
+YOLACT_REPO='./crow_vision_yolact'
+DATASET='fixedcolor'
 
 module purge
 module load CUDA/10.1.105-GCC-8.2.0-2.31.1
 module load cuDNN
 module load Anaconda3/5.0.1
-nvidia-smi
 . /opt/apps/software/Anaconda3/5.0.1/etc/profile.d/conda.sh
 conda activate yolact-env
-python ./crow_vision_yolact/train.py --batch_size=2 --dataset_number=fixedcolor
 
+nvidia-smi
+echo "YOLACT trainig started!"
+python ${YOLACT_REPO}/train.py --batch_size=2 --dataset_number=${DATASET}
+echo "done"
