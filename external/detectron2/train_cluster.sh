885c39cdd03e83e3368f20474beffa126e3b7505
Marek Otahal <markotahal@gmail.com>
2020-07-08 15:56:39 +0200

    Detectron: update cluster script for multi GPU
    specifies LR, batch, num GPUs,..
    Also fix cluster modules needed to be loaded.

--- external/detectron2/train_cluster.sh
+++ external/detectron2/train_cluster.sh
@@ -8,7 +8,8 @@
 #SBATCH --partition=gpu
 
 module purge
-module load CUDA/9.2.88-GCC-7.3.0-2.30
+module load Python/3.7.4-GCCcore-8.3.0
+module load CUDA
 module load cuDNN
 module load Anaconda3/5.0.1
 source /opt/apps/software/Anaconda3/5.0.1/etc/profile.d/conda.sh
@@ -17,5 +18,7 @@ conda activate detectron2-env
 nvidia-smi
 
 echo "Running detectron2 training..."
-python train_crow.py --config-file ./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml
+python train_crow.py --config-file  --config-file ./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml \
+  --num-gpus 3 SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.0025
+
 echo "done"
