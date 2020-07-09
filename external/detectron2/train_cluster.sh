beb96b2bcbc5186ece0dc5b4e67fab4508f0d44e
Ing. Marek Otáhal <otahama2@node-head.ciirc.cvut.cz>
2020-07-09 15:15:22 +0200

    updated cluster scripts for detectron
    WIP still not working

--- external/detectron2/train_cluster.sh
+++ external/detectron2/train_cluster.sh
@@ -2,23 +2,39 @@
 #SBATCH --job-name=train_crow_detectron2
 #SBATCH --output=train_nn_%A.log
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:3
-#SBATCH --mem=36G
-#SBATCH --time=72:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=12G
+#SBATCH --time=0:10:00
 #SBATCH --partition=gpu
 
 module purge
-module load Python/3.7.4-GCCcore-8.3.0
-module load CUDA
-module load cuDNN
-module load Anaconda3/5.0.1
-source /opt/apps/software/Anaconda3/5.0.1/etc/profile.d/conda.sh
-conda activate detectron2-env
+module load gcccuda/2020
+#module load cuDNN
+module load Anaconda3/5.3.0
+source /opt/apps/software/Anaconda3/5.3.0/etc/profile.d/conda.sh
+
+CROW_VISION_REPO="~/crow_vision"
+DETECTRON_REPO="~/detectron2"
 
 nvidia-smi
+echo "Installing.."
+cd ~
+conda env create -f ${CROW_VISION_REPO}/external/detectron2/environment.yml
+conda env update -f ${CROW_VISION_REPO}/external/detectron2/environment.yml
+conda activate detectron2-env
+
+conda info
+module list
+python --version
+conda list|grep torch
+python -c 'import torch; print(torch.cuda.is_available())' || exit 1
+
+python -m pip install --user ${DETECTRON_REPO}
 
 echo "Running detectron2 training..."
-python train_crow.py --config-file  --config-file ./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml \
-  --num-gpus 3 SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.0025
+#python detectron2/train_crow.py 
+python ${DETECTRON_REPO}/tools/train_net.py --config-file  --config-file ${CROW_VISION_REPO}/external/detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml \
+  --num-gpus 1 SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.0025 
+nvidia-smi 
 
 echo "done"
--- external/detectron2/train_crow.py
+++ external/detectron2/train_crow.py
@@ -2,10 +2,11 @@
 #
 # Run as: 
 # nohup python train_crow.py --config-file configs/COCO-InstanceSegmentation/coco_instance_segm_mask_rcnn_R_50_FNP_3x_kuka.yaml --resume &
-
+import os
 from detectron2.data.datasets import load_coco_json, register_coco_instances
 
 DATASET="/nfs/projects/crow/data/yolact/datasets/dataset_kuka_env_pybullet_fixedcolor/"
+DETECTRON_REPO=os.path.expanduser("~/detectron2")
 
 register_coco_instances(
     "kuka_train",
@@ -20,7 +21,7 @@ register_coco_instances(
     DATASET+"/test",
 )
 
-exec(open("./tools/train_net.py").read(), globals())  # TODO better pass args
+exec(open(DETECTRON_REPO+"/tools/train_net.py").read(), globals())  # TODO better pass args
 
 # from predictor import VisualizationDemo
 # exec(open("./demo/demo.py").read(), globals()) #TODO better pass args
