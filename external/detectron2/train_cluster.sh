36169b9723218ddeceb1944c6f4563ce6ae5c317
Ing. Marek Otáhal <otahama2@node-head.ciirc.cvut.cz>
2020-07-10 09:43:18 +0200

    cleanup cluster script

--- external/detectron2/train_cluster.sh
+++ external/detectron2/train_cluster.sh
@@ -4,37 +4,35 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
 #SBATCH --mem=12G
-#SBATCH --time=0:10:00
+#SBATCH --time=72:00:00
 #SBATCH --partition=gpu
 
 module purge
 module load gcccuda/2020
 #module load cuDNN
 module load Anaconda3/5.0.1
-source /opt/apps/software/Anaconda3/5.0.1/etc/profile.d/conda.sh
 
 CROW_VISION_REPO="/home/$USER/crow_vision"
-DETECTRON_REPO="/home/$USER/detectron2"
 
-nvidia-smi
-echo "Installing.."
+echo "Setup conda env"
+source /opt/apps/software/Anaconda3/5.0.1/etc/profile.d/conda.sh
 cd ~
-conda env create -f ${CROW_VISION_REPO}/external/detectron2/environment.yml
-conda env update -f ${CROW_VISION_REPO}/external/detectron2/environment.yml
+#conda env remove -n detectron2-env
+conda env create -f ${CROW_VISION_REPO}/external/detectron2/environment.yml || conda env update -f ${CROW_VISION_REPO}/external/detectron2/environment.yml
 conda activate detectron2-env
+#python -m pip install --user detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu102/index.html
 
+
+echo "debug:"
+nvidia-smi
 conda info
 module list
 python --version
 conda list|grep torch
 python -c 'import torch.cuda; print(torch.cuda.is_available())' || exit 1
 
-python -m pip install --user ${DETECTRON_REPO}
 
 echo "Running detectron2 training..."
-#python detectron2/train_crow.py 
-python ${DETECTRON_REPO}/tools/train_net.py --config-file ${CROW_VISION_REPO}/external/detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml \
-  --num-gpus 1 SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.0025 
-nvidia-smi 
+python ${CROW_VISION_REPO}/external/detectron2/train_net.py --config-file ${CROW_VISION_REPO}/external/detectron2/configs/COCO-InstanceSegmentation/crow_mask_rcnn_R_50_FPN_3x_giou.yaml
 
 echo "done"
