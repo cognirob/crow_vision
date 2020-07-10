b29fc51fba46e1f011a7a2d86afb39362130af2f
Marek Otahal <marek.otahal@cvut.cz>
2020-07-10 10:31:41 +0200

    cluster multi GPU training ON
    running on 4 GPUs

--- external/detectron2/train_cluster.sh
+++ external/detectron2/train_cluster.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=train_crow_detectron2
 #SBATCH --output=train_nn_%A.log
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:1
-#SBATCH --mem=12G
-#SBATCH --time=72:00:00
+#SBATCH --gres=gpu:4
+#SBATCH --mem=48G
+#SBATCH --time=8:00:00
 #SBATCH --partition=gpu
 
 module purge
@@ -35,6 +35,7 @@ python -c 'import torch.cuda; print(torch.cuda.is_available())' || exit 1
 echo "Running detectron2 training..."
 python ${CROW_VISION_REPO}/external/detectron2/train_net.py \
   --config-file "${CROW_VISION_REPO}/external/detectron2/configs/COCO-InstanceSegmentation/crow_mask_rcnn_R_50_FPN_3x_giou.yaml" \
-  --dataset "/nfs/projects/crow/data/yolact/datasets/dataset_kuka_env_pybullet_fixedcolor/"
+  --dataset "/nfs/projects/crow/data/yolact/datasets/dataset_kuka_env_pybullet_fixedcolor/" \
+  --num-gpus 4
 
 echo "done"
