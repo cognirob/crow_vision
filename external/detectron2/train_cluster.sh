856f3452769bdbc5ee1f35ecb71aab11a439cb70
Marek Otahal <marek.otahal@cvut.cz>
2020-08-11 19:35:24 +0200

    debugging slurm script

--- external/detectron2/train_cluster.sh
+++ external/detectron2/train_cluster.sh
@@ -2,14 +2,15 @@
 #SBATCH --job-name=train_crow_detectron2
 #SBATCH --output=train_nn_%A.log
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:8
-#SBATCH --mem=48G
-#SBATCH --time=0:10:00
+#SBATCH --gres=gpu:Volta100:2 #Note: on K40 pytorch fails #TODO cluster kept failing at multi-gpus? 
+#SBATCH --mem=32G
+#SBATCH --time=0:20:00
 #SBATCH --partition=gpu
 
+
 module purge
-module load gcccuda/2020
-#module load cuDNN
+module load gcccuda/2019b
+#module load cuDNN/7.6.4.38-gcccuda-2019b #cuda 10.1
 module load Anaconda3/5.0.1
 
 CROW_VISION_REPO="/home/$USER/crow_vision"
@@ -21,8 +22,10 @@ cd ~
 #conda env remove -n detectron2-env
 conda env create -f ${CROW_VISION_REPO}/external/detectron2/environment.yml || conda env update -f ${CROW_VISION_REPO}/external/detectron2/environment.yml
 conda activate detectron2-env
-#python -m pip install --user detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu102/index.html
-
+python -m pip install --user detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/index.html
+#cd detectron2
+#pip install -e .
+#cd ~
 
 echo "debug:"
 nvidia-smi
@@ -30,6 +33,7 @@ conda info
 module list
 python --version
 conda list|grep torch
+export NCCL_DEBUG="INFO"
 python -c 'import torch.cuda; print(torch.cuda.is_available())' || exit 1
 
 
@@ -37,6 +41,6 @@ echo "Running detectron2 training..."
 python ${CROW_VISION_REPO}/external/detectron2/train_net.py \
   --config-file "${CROW_VISION_REPO}/external/detectron2/configs/COCO-InstanceSegmentation/crow.yaml" \
   --dataset "${DATASET}" \
-  --num-gpus 8
+  --num-gpus 2 SOLVER.IMS_PER_BATCH $((28))
 
 echo "done"
