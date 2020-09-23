abde4dc6e96e97490633bacca3c80455e4e0a088
Marek Otahal <marek.otahal@cvut.cz>
2020-09-23 17:26:47 +0200

    matcher: honor min_support
    min_support is a number of points in the pointcloud that must exist in
    a) downsampled pcl, b) found registration match. Ie. the higher, the
    higher can we trust the match is correct.

--- ros2/src/crow_vision_ros2/crow_vision_ros2/match3d.py
+++ ros2/src/crow_vision_ros2/crow_vision_ros2/match3d.py
@@ -57,31 +57,24 @@ class Match3D(Node):
           orig_pcd.colors = mesh.vertex_colors
           orig_pcd.normals = mesh.vertex_normals
 
-          orig_pcd, orig_fpfh = self.preprocess_point_cloud(orig_pcd, voxel_size=self.voxel_size, label="proto"+cls)
+          orig_pcd, orig_fpfh = self.preprocess_point_cloud(orig_pcd, voxel_size=self.voxel_size, min_support=self.min_support, label="proto"+cls)
           objects[cls]["orig"] = orig_pcd
           objects[cls]["orig_fpfh"] = orig_fpfh #features for RANSAC
 
-          #2.1 optimize pcd size (filter mesh) to reduce num points, for faster ICP matching
-          down, down_fpfh = self.preprocess_point_cloud(orig_pcd, self.approx_precision, label="proto"+cls)
-          objects[cls]["down"] = down
-          objects[cls]["down_fpfh"] = down_fpfh #features for RANSAC
-
-          fine, fine_fpfh = self.preprocess_point_cloud(orig_pcd, self.fine_precision, label="proto"+cls)
-          objects[cls]["fine"] = fine
-          objects[cls]["fine_fpfh"] = fine_fpfh #features for ICP
-
-          self.get_logger().info("Loading '{}' : mesh: {}\tPointCloud: {}\tReduced pointcloud: {}\tFine cloud: {}".format(cls, mesh, orig_pcd, down, fine))
+          self.get_logger().info("Loading '{}' : mesh: {}\tPointCloud: {}".format(cls, mesh, orig_pcd))
         return objects
 
 
 
-    def __init__(self, node_name="match3d", voxel_size=0.001, approx_precision=0.050, fine_precision=0.005):
+    def __init__(self, node_name="match3d", voxel_size=0.001, approx_precision=0.050, fine_precision=0.005, min_support=200): #TODO make a) finer STLs (for some objects), b) merge pcl to get bigger clouds, so support can be much higher (~1000 ideally)
         """
         @arg voxel_size: size [in m] of voxels used for sampling from our ground-truth models. Default 0.001 = 1mm in real world precision. 
             Defines overall precision-possibilities of this module. 
         @arg approx_precision: precision in voxel_size (= in m) for global registration method (RANSAC). This is used to get a rough estimate of the object's
             position. Default 0.050 is 5cm. Use 0.0 to disable this method.
         @arg fine_precision: same as approx_precision but for local registration method (ICP). Default 0.005 = 5mm, set to 0.0 to disable.
+        @arg min_support: min number of downsampled points in pointclouds (both "proto" models and "incoming" segmented pcl). Also used as threshold for acceptance in registration's
+            result (the higher the support, the more sound the match is).
         """
         super().__init__(node_name)
         self.approx_precision = approx_precision
@@ -92,6 +85,7 @@ class Match3D(Node):
         if fine_precision > 0:
             assert voxel_size < fine_precision
         self.fine_precision = fine_precision
+        self.min_support = min_support
 
         self.cameras = []
         while(len(self.cameras) == 0):
@@ -137,10 +131,11 @@ class Match3D(Node):
             MODEL_PATH+"wheel.stl", 
             MODEL_PATH+"peg_screw.stl", 
             MODEL_PATH+"wrench.stl", 
-            MODEL_PATH+"peg_simple.stl" 
+       #     MODEL_PATH+"peg_simple.stl" #has too few points (36), making our min_support useless
             ], 
             list_labels=["car_roof", "pliers", "cube_holes", "screw_round", "ex_bucket", "screwdriver", "hammer", 
-                "sphere_holes", "wafer", "nut", "wheel", "peg_screw", "wrench", "peg_simple"
+                "sphere_holes", "wafer", "nut", "wheel", "peg_screw", "wrench"
+                #, "peg_simple"
             ] 
             )
 
@@ -159,22 +154,20 @@ class Match3D(Node):
                                       up=[-0.2779, -0.9482 ,0.1556])
 
 
-    def preprocess_point_cloud(self, pcd, voxel_size, min_support = 30, label="n/a"): #TODO make finer grained STLs so support can be much higher! (~1000)
+    def preprocess_point_cloud(self, pcd, voxel_size, min_support, label="n/a"): 
         """
         @arg pcd: o3d.PointCloud data
         @arg voxel_size: size of voxel meant for downsampling (->precision)
-        @arg min_support: min threshold on number of points after the downsampling 
+        @arg min_support: min threshold on number of points after the downsampling (->trustfullness of the match)
         """
         #print(":: Downsample with a voxel size %.3f." % voxel_size)
         assert voxel_size >= self.voxel_size
 
         pcd_down = copy.deepcopy(pcd)
         downsampled = pcd.voxel_down_sample(voxel_size)
-        if len(downsampled.points) > min_support:
-          pcd_down = downsampled #actually downsampled data
-        else:
-            self.get_logger().warn("Did not manage to downsample '{}' -  {} to voxel_size {} with support {}, only has {} support points.".format(
-                label, pcd, voxel_size, min_support, len(downsampled.points) ))
+        assert len(downsampled.points) > min_support, "Did not manage to downsample '{}' -  {} to voxel_size {} with support {}, only has {} support points.".format(
+                label, pcd, voxel_size, min_support, len(downsampled.points))
+        pcd_down = downsampled #actually downsampled data
 
         radius_normal = voxel_size * 2 #TODO should we tune these *X numbers?
         #print(":: Estimate normal with search radius %.3f." % radius_normal)
@@ -186,14 +179,13 @@ class Match3D(Node):
         return pcd_down, pcd_fpfh
 
 
-    def execute_fast_global_registration(self, source_down, target_down, source_fpfh, target_fpfh, voxel_size):
-        distance_threshold = voxel_size * 0.5
+    def execute_fast_global_registration(self, source_down, target_down, source_fpfh, target_fpfh, match_tolerance):
+        assert match_tolerance > self.voxel_size
         #print(":: Apply fast global registration with distance threshold %.3f" % distance_threshold)
         result = o3d.registration.registration_fast_based_on_feature_matching(
             source_down, target_down, source_fpfh, target_fpfh, o3d.registration.FastGlobalRegistrationOption(
-                maximum_correspondence_distance=distance_threshold))
+                maximum_correspondence_distance=match_tolerance))
         return result
-
 
 
 
@@ -215,7 +207,13 @@ class Match3D(Node):
         real_pcl = o3d.geometry.PointCloud()
         #real_pcl.points = o3d.utility.Vector3dVector(ros_numpy.point_cloud2.pointcloud2_to_xyz_array(msg.pcl)) #using ros2_numpy 
         real_pcl.points = o3d.utility.Vector3dVector(pcd)
+        # downsample to self.voxel_size resolution
+        real_pcl, _ = self.preprocess_point_cloud(real_pcl, voxel_size=self.voxel_size, min_support=0, label="incoming-"+msg.label)
         end = time()
+        if len(real_pcl.points) < self.min_support:
+            self.get_logger().warn("Skipping [{}] incoming pointcloud {} as it has too few points (min_support={}).".format(
+                msg.label, real_pcl, self.min_support))
+            return
         #self.get_logger().info("Convert to o3d: {}".format(end-start)) # ~0.003s
 
 
@@ -240,19 +238,18 @@ class Match3D(Node):
         if self.approx_precision > 0:
           start = time()
           # compute pcl & features for incoming segmented pcl (this is the target position)
-          target_down, target_fpfh = self.preprocess_point_cloud(real_pcl, self.approx_precision, label="real-approx-"+msg.label)
+          target_down, target_fpfh = self.preprocess_point_cloud(real_pcl, voxel_size=self.voxel_size, min_support=self.min_support, label="real-approx-"+msg.label)
           # a) re-compute pcl & features for the model (which moved in step 0)
-          source_down, source_fpfh = self.preprocess_point_cloud(model_pcl,  self.approx_precision, label="proto-approx-"+msg.label)
+          source_down, source_fpfh = self.preprocess_point_cloud(model_pcl,voxel_size=self.voxel_size, min_support=self.min_support, label="proto-approx-"+msg.label)
           # b) use cached downsampled model & features #TODO can we do that? or model_fpfh depends on absolute coords, which changed in step 0/ default transform?
           #source_down = self.objects[msg.label]["down"]
           #source_fpfh = self.objects[msg.label]["down_fpfh"]
-          result = self.execute_fast_global_registration(source_down, target_down, source_fpfh, target_fpfh, self.approx_precision)
+          result = self.execute_fast_global_registration(source_down, target_down, source_fpfh, target_fpfh, self.approx_precision*10)
           end = time()
 
           #apply the transform only if: 1) cprrespondence_set is atleast 50% of the segmented pcl (real_pcl) & fitness > 0.1
           #print("diff {}\tlen orig: {}\tlen match: {}".format(float(len(result.correspondence_set) / len(target_down.points)), len(target_down.points), len(result.correspondence_set)))
-          applyit = (float(len(result.correspondence_set)) / len(target_down.points)) > 0.25 and \
-                    len(result.correspondence_set) > 10 and result.fitness > 0.01
+          applyit = (float(len(result.correspondence_set)) / len(target_down.points)) > 0.25 and len(result.correspondence_set) > self.min_support and result.fitness > 0.01
           self.get_logger().info("RANSAC [{}]: {}\t in {}sec - {}".format(msg.label, result, (end-start),  "APPLIED" if applyit else "SKIPPED"))
           if applyit:
               model_pcl.transform(result.transformation)
@@ -268,9 +265,9 @@ class Match3D(Node):
         if self.fine_precision > 0:
           start = time()
           # compute pcl & features for incoming segmented pcl (this is the target position)
-          target_fine, target_fpfh = self.preprocess_point_cloud(real_pcl, self.fine_precision, label="real-fine-"+msg.label)
+          target_fine, target_fpfh = self.preprocess_point_cloud(real_pcl, voxel_size=self.voxel_size, min_support=self.min_support, label="real-fine-"+msg.label) #TODO if we wanted to alter min_support based on object's label (-> "size"). As hammer "is bigger" than "nut"
           # a) re-compute pcl & features for the model (which moved in step 0)
-          source_fine, source_fpfh = self.preprocess_point_cloud(model_pcl,  self.fine_precision, label="proto-fine-"+msg.label)
+          source_fine, source_fpfh = self.preprocess_point_cloud(model_pcl,voxel_size=self.voxel_size, min_support=self.min_support, label="proto-fine-"+msg.label)
           # b) use cached downsampled model & features #TODO can we do that? or model_fpfh depends on absolute coords, which changed in step 0/ default transform?
           #source_fine = self.objects[msg.label]["fine"]
           #source_fpfh = self.objects[msg.label]["fine_fpfh"]
@@ -281,13 +278,13 @@ class Match3D(Node):
           result = o3d.registration.registration_icp(
               source= source_fine,
               target= target_fine, 
-              max_correspondence_distance=0.1, #TODO what units is this?
+              max_correspondence_distance=self.fine_precision, #match tolerance in mm
               #init=result_ransac.transformation, #TODO bundle the TF from locator to SegmentedPointcloud and a) transform model_pcl to the real_pcl's close location; or b) provide the init as 4x4 float64 initial transform estimation (better?) #TODO2 not needed now as we move the model ourselves?
               estimation_method=o3d.registration.TransformationEstimationPointToPoint())
           end = time()
 
           #apply the transform only if: 1) correspondence_set is atleast 50% of the segmented pcl (real_pcl) & fitness > 0.1
-          applyit = (float(len(result.correspondence_set)) / len(target_fine.points)) > 0.1 and len(result.correspondence_set) > 50 and result.fitness > 0.1
+          applyit = (float(len(result.correspondence_set)) / len(target_fine.points)) > 0.1 and len(result.correspondence_set) > self.min_support and result.fitness > 0.1
           self.get_logger().info("ICP [{}]: {}\t in {}sec - {}".format(msg.label, result, (end-start),  "APPLIED" if applyit else "SKIPPED"))
           if applyit:
               matched = True
