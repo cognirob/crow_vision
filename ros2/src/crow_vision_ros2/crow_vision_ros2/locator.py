a130b10da24f3632797dd3b7cc1550180498fba5
Marek Otahal <marek.otahal@cvut.cz>
2020-09-23 13:22:15 +0200

    logger: fix crash when no masks detected

--- ros2/src/crow_vision_ros2/crow_vision_ros2/locator.py
+++ ros2/src/crow_vision_ros2/crow_vision_ros2/locator.py
@@ -73,7 +73,7 @@ class Locator(Node):
     def detection_callback(self, pcl_msg, mask_msg, camera):
         #print(self.getCameraData(camera))
         if not mask_msg.masks:
-            self.get_logger.info("no masks, no party. Quitting early.")
+            self.get_logger().info("no masks, no party. Quitting early.")
             return  # no mask detections (for some reason)
 
         cameraData = self.getCameraData(camera)
