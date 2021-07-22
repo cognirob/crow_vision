9614517f4b461876bdf280c2f454880e8ab160de
Radoslav Skoviera <radoslav.skoviera@cvut.cz>
2021-07-22 13:05:57 +0200

    Delete, was just temporary

--- ros2/src/crow_vision_ros2/crow_vision_ros2/utils/tester_cam_trans.py
+++
@@ -1,9 +0,0 @@
-from get_camera_transformation import get_camera_transformation
-
-#unknown serial nr
-res = get_camera_transformation('aaa')
-print(res)
-
-#known serial nr
-res = get_camera_transformation('cam2')
-print(res)
