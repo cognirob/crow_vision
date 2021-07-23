7ef3ca6e0db2491ecfcdedc26f45e2e8a058af7f
Your Name <you@example.com>
2021-07-23 14:33:04 +0200

    detector profiling

--- ros2/src/crow_vision_ros2/crow_vision_ros2/detector.py
+++ ros2/src/crow_vision_ros2/crow_vision_ros2/detector.py
@@ -18,6 +18,8 @@ import commentjson as json
 import pkg_resources
 import time
 import copy
+from crow_control.utils.profiling import StatTimer
+
 
 print(f"Running PyTorch:")
 print(f"\tver: {torch.__version__}")
@@ -129,6 +131,7 @@ class CrowVision(Node):
         assert os.path.exists(model_abs), "Provided path to model weights does not exist! {}".format(model_abs)
         self.cnn = InfTool(weights=model_abs, top_k=self.config["top_k"], score_threshold=self.config["threshold"], config=cfg)
         print('Hi from crow_vision_ros2.')
+        StatTimer.init()
 
     def input_callback(self, msg, topic):
         """
@@ -144,17 +147,24 @@ class CrowVision(Node):
         # self.get_logger().error(str(dir(self.ros[topic]["pub_masks"])))
         # assert topic in self.ros, "We don't have registered listener for the topic {} !".format(topic)
 
+        StatTimer.enter("full detection")
+        StatTimer.enter("imgmsg_to_cv2")
         img_raw = self.cvb_.imgmsg_to_cv2(msg, "bgr8")
+        StatTimer.exit("imgmsg_to_cv2")
         #img_raw = cv2.cvtColor(img_raw, cv2.COLOR_BGR2RGB)
 
+        StatTimer.enter("infer")
         preds, frame = self.cnn.process_batch(img_raw)
+        StatTimer.exit("infer")
         if preds[0]["detection"] is None:
             return  # do not publish if nothing was detected
 
         #the input callback triggers the publishers here.
         if "pub_img" in self.ros[topic]: # labeled image publisher. (Use "" to disable)
+            StatTimer.enter("annotate")
             img_labeled = self.cnn.label_image(img_raw, copy.deepcopy(preds), copy.deepcopy(frame))
             img_labeled = cv2.cvtColor(img_labeled, cv2.COLOR_BGR2RGB)
+            StatTimer.exit("annotate")
 
             if img_labeled.ndim == 3:
                 batch,w,h,c = 1, *img_labeled.shape
@@ -168,10 +178,14 @@ class CrowVision(Node):
             msg_img.header.stamp = msg.header.stamp #we always inherit timestamp from the original "time taken", ie stamp from camera topic
             msg_img.header.frame_id = msg.header.frame_id  # TODO: fix frame name because stupid Intel RS has only one frame for all cameras
             #   self.get_logger().info("Publishing as Image {} x {}".format(msg_img.width, msg_img.height))
+            StatTimer.enter("pub annot img")
             self.ros[topic]["pub_img"].publish(msg_img)
+            StatTimer.exit("pub annot img")
 
         if "pub_masks" in self.ros[topic] or "pub_bboxes" in self.ros[topic]:
+            StatTimer.enter("compute masks")
             classes, class_names, scores, bboxes, masks, centroids = self.cnn.raw_inference(img_raw, preds, frame)
+            StatTimer.exit("compute masks")
             classes = classes.astype(int).tolist()
             scores = scores.astype(float).tolist()
             if len(classes) == 0:
@@ -179,6 +193,7 @@ class CrowVision(Node):
                 return
 
             if "pub_masks" in self.ros[topic]:
+                StatTimer.enter("process & pub masks")
                 msg_mask = DetectionMask()
                 m_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
                 msg_mask.masks = [self.cvb_.cv2_to_imgmsg(cv2.morphologyEx(mask, cv2.MORPH_OPEN, m_kernel), encoding="mono8") for mask in masks.astype(np.uint8)]
@@ -193,6 +208,7 @@ class CrowVision(Node):
                 msg_mask.scores = scores
                 #self.get_logger().info("Publishing as String {} at time {} ".format(msg_mask.class_names, msg_mask.header.stamp.sec))
                 self.ros[topic]["pub_masks"].publish(msg_mask)
+                StatTimer.exit("process & pub masks")
             if "pub_bboxes" in self.ros[topic]:
                 msg_bbox = DetectionBBox()
                 msg_bbox.bboxes = [BBox(bbox=bbox) for bbox in bboxes]
@@ -204,6 +220,7 @@ class CrowVision(Node):
                 msg_bbox.scores = scores
                 # self.get_logger().info("Publishing as String {} at time {} ".format(msg_mask.data, msg_mask.header.stamp.sec))
                 self.ros[topic]["pub_bboxes"].publish(msg_bbox)
+        StatTimer.try_exit("full detection")
 
 
 def main(args=None):
