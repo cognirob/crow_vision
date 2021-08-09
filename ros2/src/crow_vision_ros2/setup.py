b307e5abffb6eafa32c7c8c694810cab4141654b
Radoslav Skoviera <radoslav.skoviera@cvut.cz>
2021-08-09 13:09:08 +0200

    Added camera transfrom extractor

--- ros2/src/crow_vision_ros2/setup.py
+++ ros2/src/crow_vision_ros2/setup.py
@@ -12,8 +12,10 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('lib/' + package_name, ['scripts/camera_transform_extractor']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.txt'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
