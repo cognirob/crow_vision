f42fc72492eb5e6360ef9570edad27971a390ac2
Your Name <you@example.com>
2021-08-20 17:26:12 +0200

    Affordance

--- ros2/src/crow_vision_ros2/launch/crow_object.launch.py
+++ ros2/src/crow_vision_ros2/launch/crow_object.launch.py
@@ -11,7 +11,31 @@ def generate_launch_description():
     launchConfigs = []
 
     #2. detector (2D vision)
-    detector_node = launch_ros.actions.Node(
+    edge_detector_node = launch_ros.actions.Node(
+        package='crow_vision_ros2',
+        node_executable='detector_edge',
+        output='log',
+        node_name="detector_edge",
+        parameters=[{
+                    "weights": "data/yolact/weights/weights_yolact_kuka_30/crow_base_25_133333.pth",
+                    "config": "data/yolact/weights/weights_yolact_kuka_30/config_train.obj"
+                    }]
+    )
+    launchConfigs.append(edge_detector_node)
+
+    # afford_detector_node = launch_ros.actions.Node(
+    #     package='crow_vision_ros2',
+    #     node_executable='detector_edge',
+    #     output='log',
+    #     node_name="afford_detector_edge",
+    #     parameters=[{
+    #                 "weights": "data/yolact/weights/weights_yolact_kuka_26/crow_base_52_133333.pth",
+    #                 "config": "data/yolact/weights/weights_yolact_kuka_26/config_train.obj"
+    #                 }]
+    # )
+    # launchConfigs.append(afford_detector_node)
+
+    pose_detector_node = launch_ros.actions.Node(
         package='crow_vision_ros2',
         node_executable='detector_pose',
         output='screen',
@@ -21,19 +45,7 @@ def generate_launch_description():
         #             "config": "data/yolact/weights/weights_yolact_kuka_30/config_train.obj"
         #             }]
     )
-    launchConfigs.append(detector_node)
-
-    detector_node = launch_ros.actions.Node(
-        package='crow_vision_ros2',
-        node_executable='detector_edge',
-        output='screen',
-        node_name="detector_edge",
-        parameters=[{
-                    "weights": "data/yolact/weights/weights_yolact_kuka_30/crow_base_25_133333.pth",
-                    "config": "data/yolact/weights/weights_yolact_kuka_30/config_train.obj"
-                    }]
-    )
-    launchConfigs.append(detector_node)
+    launchConfigs.append(pose_detector_node)
 
     #3. locator (3D segmented pcl)
     locator_node = launch_ros.actions.Node(
