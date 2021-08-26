7010db1615a989104f790cab7324cab1c3906e23
Radoslav Skoviera <radoslav.skoviera@cvut.cz>
2021-08-26 18:19:21 +0200

    Launch options

--- ros2/src/crow_vision_ros2/launch/crow_object.launch.py
+++ ros2/src/crow_vision_ros2/launch/crow_object.launch.py
@@ -1,5 +1,6 @@
-from launch import LaunchDescription
 import launch_ros.actions
+from launch.conditions import IfCondition, UnlessCondition
+from launch import LaunchDescription, LaunchContext
 from launch.actions import LogInfo, IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration
@@ -10,11 +11,18 @@ from ros2topic import api
 def generate_launch_description():
     launchConfigs = []
 
+    # context = LaunchContext()
+    use_edge = LaunchConfiguration("use_edge")
+    edge_arg = DeclareLaunchArgument("use_edge", default_value="True")
+    use_pose = LaunchConfiguration("use_pose")
+    pose_arg = DeclareLaunchArgument("use_pose", default_value="True")
+
     #2. detector (2D vision)
     edge_detector_node = launch_ros.actions.Node(
         package='crow_vision_ros2',
         node_executable='detector_edge',
         output='log',
+        condition=IfCondition(use_edge),
         node_name="detector_edge",
         parameters=[{
                     "weights": "data/yolact/weights/weights_yolact_kuka_30/crow_base_25_133333.pth",
@@ -22,10 +30,22 @@ def generate_launch_description():
                     }]
     )
     launchConfigs.append(edge_detector_node)
+    detector_node = launch_ros.actions.Node(
+        package='crow_vision_ros2',
+        node_executable='detector',
+        output='log',
+        condition=UnlessCondition(use_edge),
+        node_name="detector",
+        parameters=[{
+                    "weights": "data/yolact/weights/weights_yolact_kuka_30/crow_base_25_133333.pth",
+                    "config": "data/yolact/weights/weights_yolact_kuka_30/config_train.obj"
+                    }]
+    )
+    launchConfigs.append(detector_node)
 
     # afford_detector_node = launch_ros.actions.Node(
     #     package='crow_vision_ros2',
-    #     node_executable='detector_edge',
+    #     node_executable=yolact_node,
     #     output='log',
     #     node_name="afford_detector_edge",
     #     parameters=[{
@@ -40,6 +60,7 @@ def generate_launch_description():
         node_executable='detector_pose',
         output='screen',
         node_name="detector_pose",
+        condition=IfCondition(use_pose)
         # parameters=[{
         #             "weights": "data/yolact/weights/weights_yolact_kuka_30/crow_base_25_133333.pth",
         #             "config": "data/yolact/weights/weights_yolact_kuka_30/config_train.obj"
@@ -68,5 +89,8 @@ def generate_launch_description():
     launchDescription = LaunchDescription(launchConfigs)
 
     return LaunchDescription([
-        DeclareLaunchArgument("camera_config", default_value="rs_native.yaml")
+        edge_arg,
+        pose_arg,
+        LogInfo(msg=["Will use Edge? ", LaunchConfiguration('use_edge')]),
+        LogInfo(msg=["Will use Pose? ", LaunchConfiguration('use_pose')])
     ] + launchConfigs)
